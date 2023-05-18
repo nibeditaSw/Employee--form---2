17663b2c8f826c9a92946d767f9d4dd091592008
nibeditaSw <nibeditaswain0285@gmail.com>
2023-05-18 19:19:26 +0530

    commit

--- sara-1.js
+++ sara-1.js
@@ -4,65 +4,62 @@ class EmployeeForm extends LitElement {
   static styles = css`
     
 
-  `;
 
+
+  `;
 
   static get properties() {
     return {
-      employees: { type: Array }
+      employees: { type: Array },
+      filteredEmployees: { type: Array }
     };
   }
 
   constructor() {
     super();
     this.employees = JSON.parse(localStorage.getItem("employees")) || [];
-    
+    this.filteredEmployees = [...this.employees];
   }
 
 
- 
-  
 
-  renderEmployeeDetails(employee) {
-    
-      return html`
-        <div class="employee-info">
+  handleSearch(e) {
+    const query = e.target.value.toLowerCase();
+    this.filteredEmployees = this.employees.filter(
+      emp => emp.fullName.toLowerCase().includes(query) || emp.employeeCode.toLowerCase().includes(query)
 
-          <p>Full Name: ${employee.fullName}</p>
-          <p>Employee Code: ${employee.employeeCode}</p>
-          
-          
-          
+    );
 
-          
-
-        </div>
+  }
 
 
-        
-      `;
-    }
-  
-  
 
-  
   render() {
     return html`
 
-    <div id="data">
-    <h1>Employee Data List</h1>
-    <ol>
-      ${this.employees.map(item => html`
-        <li>
-          ${this.renderEmployeeDetails(item)}
-        </li>
-      `)}
-    </ol>
-  </div>
- `;
+      <div id="search-bar">
+        <label for="search-input">Search:</label>
+        <input type="text" placeholder="Search.." id="search-input" @input="${this.handleSearch}" />
+        <!-- <button @click="${this.handleSearch}">Search</button> -->
+      </div>
 
- }
+      <div id="data">
+        <h1>Employee Data List</h1>
+        <ol>
+          ${this.filteredEmployees.map(item => html`
+            <li>
+              <div class="employee-info">
+              
+                <p>Full Name: ${item.fullName}</p>
+                <p>Employee Code: ${item.employeeCode}</p>
 
+              </div>
+            </li>
+          `)}
+        </ol>
+      </div>
+    `;
+  }
 }
 
 customElements.define('sara-1', EmployeeForm);
