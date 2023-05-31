5bcd0e4e44f9567d664d1d7711aae8441325bfb3
nibeditaSw <nibeditaswain0285@gmail.com>
2023-05-31 19:34:51 +0530

    commit

--- nehu-3.js
+++ nehu-3.js
@@ -1,4 +1,14 @@
 import { html, css, LitElement } from 'lit';
+import { html, css, LitElement } from 'lit';
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import '@shoelace-style/shoelace/dist/components/button/button.js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import '@shoelace-style/shoelace/dist/components/input/input.js';
+import '@shoelace-style/shoelace/dist/components/rating/rating.js';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+setBasePath('/path/to/shoelace/dist');
+
+
 
 
 class EmployeeForm extends LitElement {
